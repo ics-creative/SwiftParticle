1a623adc658e549fd86f00d266685074a0178f02
ics-kano <takeshi.kano@ics-web.jp>
2015-03-25 19:01:10 +0900

    パーティクルブラッシュアップその2

--- SwiftParticle/GameScene.swift
+++ SwiftParticle/GameScene.swift
@@ -8,80 +8,122 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    /** パーティクル発生装置 */
-    var particleEmitter:SKEmitterNode = SKEmitterNode(fileNamed: "MyParticle.sks");
-    var smokeParticleEmitter:SKEmitterNode = SKEmitterNode(fileNamed: "MyParticle2.sks");
-
-    /** 発生装置の現在地 */
-    var currentPoint:CGPoint = CGPoint(x: 0, y: 0);
-    /** 発生装置の目的地 */
-    var goalPoint:CGPoint = CGPoint(x: 0, y: 0);
-    /** パーティクルの色相。時間とともに変化する */
+    /** 1つのパーティクル発生装置から発生するパーティクルの数 */
+    var particleNum:CGFloat = 0;
+    /** パーティクル発生装置の数 */
+    let EMITTER_NUM:Int = 8;
+    /** 煙パーティクル発生装置の数 */
+    let SMOKE_EMMITER_NUM:Int = 3;
+    /** パーティクル発生装置をまとめた配列 */
+    var emmiterSet:[SKEmitterNode] = [SKEmitterNode]();
+    /** パーティクルの発生位置 */
+    var emmitPoint:CGPoint = CGPoint(x: 0, y: 0);
+    /** パーティクル発生装置の数 */
     var hue:CGFloat = 0;
+    // そのほかのパーティクルの色情報
     var s:CGFloat = 80;
     var v:CGFloat = 2;
     var a:CGFloat = 1;
+    /** タッチ中かどうか */
+    var isTouch:Bool = false;
+    /** タッチ箇所の数 */
+    var touchCount:Int = 0;
     
     /** 画面が表示された時に実行される */
     override func didMoveToView(view: SKView) {
         // 背景色の設定
         self.backgroundColor = UIColor(red: 0.01, green: 0.01, blue: 0.1, alpha: 1);
-        // パーティクル発生装置を配置
-        addChild(particleEmitter);
-        addChild(smokeParticleEmitter);
+        // 光のパーティクルの発生装置
+        var particleEmitter:SKEmitterNode = SKEmitterNode(fileNamed: "MyParticle.sks");
+        particleNum = particleEmitter.particleBirthRate;
+        // 煙のパーティクルの発生装置
+        var smokeEmitter:SKEmitterNode = SKEmitterNode(fileNamed: "MySmoke.sks");
+        // 両方のパーティクル発生装置を配列に格納していく。
+        for var i:Int = 0; i < EMITTER_NUM; i++ {
+            var copiedEmmiter:SKEmitterNode!;
+            if(i < EMITTER_NUM - SMOKE_EMMITER_NUM)
+            {
+                copiedEmmiter = particleEmitter.copy() as SKEmitterNode;
+            }
+            else
+            {
+                copiedEmmiter = smokeEmitter.copy() as SKEmitterNode;
+            }
+            emmiterSet.append(copiedEmmiter);
+            // 作成したパーティクル発生装置を画面に配置
+            addChild(copiedEmmiter);
+        }
     }
     
     /**
-    タッチが開始したときに実行される関数
+    画面更新毎に実行される関数。色と発生位置を変更する。
     */
     override func update(currentTime: CFTimeInterval) -> Void
     {
-        let difX:CGFloat = goalPoint.x - currentPoint.x;
-        let difY:CGFloat = goalPoint.y - currentPoint.y;
-        currentPoint.x += difX * 0.9;
-        currentPoint.y += difY * 0.9;
-        particleEmitter.particlePosition = currentPoint;
-        smokeParticleEmitter.particlePosition = currentPoint;
-        
-        hue += 0.004;
+        hue += 0.002;
         if(hue > 1)
         {
             hue = 0;
         }
         
         let color:UIColor = UIColor(hue: hue, saturation: s, brightness: v, alpha:a);
-        particleEmitter.particleColor = color;
-        smokeParticleEmitter.particleColor = color;
+        
+        for emitter:SKEmitterNode in emmiterSet {
+            emitter.particleColor = color;
+            emitter.particlePosition = emmitPoint;
+            if(!isTouch && emitter.particleBirthRate > 0)
+            {
+                emitter.particleBirthRate = 0;
+            }
+            else if(isTouch && emitter.particleBirthRate == 0)
+            {
+                emitter.particleBirthRate = particleNum;
+            }
+            emitter.alpha = CGFloat(arc4random() % 1000) / 1000;
+        }
+        
     }
     
     /**
     タッチが開始したときに実行される関数
     */
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        touchHandler(touches);
+        changeEmmitPointHandler(touches);
+        var touchedCount:Int = touches.allObjects.count;
+        touchCount = touchedCount;
+        touchesCountChangeHandler();
     }
     
     /**
     タッチが移動したときに実行される関数
     */
     override func touchesMoved(touches: NSSet, withEvent event: UIEvent) {
-        touchHandler(touches);
+        changeEmmitPointHandler(touches);
     }
     
     /**
     タッチを終えたときに実行される関数
     */
     override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
-        
+        var touchEndCount:Int = touches.allObjects.count;
+        touchCount = touchCount - touchEndCount;
+        touchesCountChangeHandler();
     }
     
     /**
-    タッチ開始、タッチ移動の時の処理
+    パーティクルの発生位置を変更する関数
     */
-    func touchHandler(touches: NSSet) -> Void {
+    func changeEmmitPointHandler(touches: NSSet) {
         for touch in touches {
             var location = touch.locationInNode(self);
-            goalPoint = location;
+            emmitPoint = location;
         }
+    }
+    
+    /**
+    タッチ中の指の数が変わった時の処理
+    */
+    func touchesCountChangeHandler() -> Void {
+        isTouch = touchCount > 0;
     }
 }
